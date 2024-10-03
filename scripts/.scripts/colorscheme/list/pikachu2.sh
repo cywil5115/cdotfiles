cfe5c4f8d28ede6d6614b2767bba9bf846993eb9
cywil5115 <116916841+cywil5115@users.noreply.github.com>
2024-10-03 23:09:54 +0200

    add sperate colors for kitty (it's better to focus more on different thinks than colors)

---
+++ scripts/.scripts/colorscheme/list/pikachu2.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+ckolor01="#fbe260"
+ckolor02="#fad61d"
+ckolor03="#fdf2bb"
+# ckolor04="#e19720"
+ckolor04="#fad61d"
+ckolor05="#ebfafa"
+ckolor06="#f62d14"
+# Markdown codeblock
+ckolor07="#1c242f"
+ckolor08="#811e09"
+# Comments
+ckolor09="#a5afc2"
+# Terminal and neovim background
+ckolor10="#0D1116"
+# Underline spellbad
+ckolor11="#7d14f6"
+# Underline spellcap
+ckolor12="#8c5103"
+# line across cursor
+ckolor13="#314154"
+# Cursor and tmux windows text
+ckolor14="#ebfafa"
+# Tmux inactive windows
+ckolor15="#013e4a"
+# Selected text
+ckolor16="#e9b3fd"
+# Lualine across
+ckolor17="#141b22"
