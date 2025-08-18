7d16fbe2340471ac517853cd77c889263e56850e
cywil5115 <116916841+cywil5115@users.noreply.github.com>
2025-08-18 12:26:07 +0200

    add yazi default theme mode when needed

--- yazi/.config/yazi/theme.sh
+++ yazi/.config/yazi/theme.sh
@@ -1,34 +1,45 @@
 #!/usr/bin/env bash
 
 yazi-choose-theme () {
-  chosen_theme=$('ls' ~/.config/yazi/flavors | sed -n 's/\.yazi$//p'| fzf --height=~15% --border=double)
-  
   yazi_theme_file="$HOME/.config/yazi/theme.toml"
 
-  cat >"$yazi_theme_file" <<EOF
+  if [[ "$1" == "default" ]]; then
+    {
+      echo ''
+      echo '# A TOML linter such as https://taplo.tamasfe.dev/ can use this schema to validate your config.'
+      echo '# If you encounter any issues, please make an issue at https://github.com/yazi-rs/schemas.'
+      echo '"$schema" = "https://yazi-rs.github.io/schemas/theme.json"'
+      echo ''
+      echo '# flavor taken from: https://github.com/dangooddd/kanagawa.yazi'
+      echo '# : Flavor {{{'
+      echo '# [flavor]'
+      echo '# dark= ""'
+      echo '# light= ""'
+    } > "$yazi_theme_file"
+  else
+    chosen_theme=$(ls ~/.config/yazi/flavors | sed -n 's/\.yazi$//p' | fzf --height=~15% --border=double)
 
-# A TOML linter such as https://taplo.tamasfe.dev/ can use this schema to validate your config.
-# If you encounter any issues, please make an issue at https://github.com/yazi-rs/schemas.
-"$schema" = "https://yazi-rs.github.io/schemas/theme.json"
-
-# flavor taken from: https://github.com/dangooddd/kanagawa.yazi
-# : Flavor {{{
-
-[flavor]
-dark= "$chosen_theme"
-light= "$chosen_theme"
-
-EOF
+    {
+      echo ''
+      echo '# A TOML linter such as https://taplo.tamasfe.dev/ can use this schema to validate your config.'
+      echo '# If you encounter any issues, please make an issue at https://github.com/yazi-rs/schemas.'
+      echo '"$schema" = "https://yazi-rs.github.io/schemas/theme.json"'
+      echo ''
+      echo '# flavor taken from: https://github.com/dangooddd/kanagawa.yazi'
+      echo '# : Flavor {{{'
+      echo '[flavor]'
+      echo "dark= \"$chosen_theme\""
+      echo "light= \"$chosen_theme\""
+    } > "$yazi_theme_file"
+  fi
 }
 
 skipline () {
-	echo " "
+  echo " "
 }
 
-yazi-choose-theme
+yazi-choose-theme "$1"
 skipline
 
 echo -e "  \033[1;30m[\033[1;32mi\033[1;30m]\033[0m Done."
-# echo "[i] Done."
-
 skipline
